3e16dcf8dd4c9ae10dde2ef1fe19676b9184f6f3
David van Munster <davidru01@live.nl>
2025-04-21 13:35:08 +0200

    Initial commit

---
+++ lua/lush_theme/theme.lua
@@ -0,0 +1,58 @@
+local lush = require("lush")
+local hsluv = lush.hsluv
+
+local fg = hsluv(60, 30, 85)
+local bg = hsluv(140, 40, 5)
+local yellow = hsluv(65, 100, 80)
+local orange = hsluv(30, 100, 60)
+local green = hsluv(120, 90, 70)
+local blue = hsluv(239, 90, 70)
+local red = hsluv(0, 90, 60)
+
+---@diagnostic disable: undefined-global
+local theme = lush(function(injected_functions)
+  local sym = injected_functions.sym
+  return {
+    -- stylua: ignore start
+
+    -- UI
+    Cursor         { bg = yellow },
+    Normal         { fg = fg, bg = bg },
+    NormalFloat    { bg = bg.da(40).de(10) },
+    FloatBorder    { fg = bg.li(60).de(70),bg = bg.da(40).de(10) },
+    FloatTitle     { fg = FloatBorder.fg.li(30), bg = bg.da(40).de(10), bold = true },
+    StatusLine     { bg = bg.li(80).de(80), fg = bg },
+    StatusLineNC   { bg = StatusLine.bg.da(30), fg = bg },
+    Tabline        { bg = StatusLine.bg.da(30) },
+    TablineSel     { bg = bg, fg = fg, bold = true },
+    WinSeparator   { fg = bg.li(60).de(80) },
+    Pmenu          { bg = bg.li(20).de(10) },
+    PmenuSel       { fg = bg, bg = yellow },
+    QuickFixLine   { PmenuSel },
+
+    Title { fg = red, bold = true },
+    DiagnosticInfo { fg = blue.li(20).de(20) },
+
+    -- Syntax
+    Delimiter      { fg = fg.de(40) },
+    Punctuation    { fg = fg },
+    Operator       { fg = fg },
+    Keyword        { fg = orange },
+    Special        { fg = fg },
+    Constant       { fg = yellow },
+    Number         { fg = green },
+    String         { fg = green },
+    Identifier     { fg = fg },
+    Function       { fg = fg },
+    Statement      { fg = fg },
+    Type           { fg = orange },
+    Directory      { fg = blue },
+    Comment        { fg = blue.da(20) },
+
+    sym"@variable" { fg = fg },
+
+    -- stylua: ignore end
+  }
+end)
+
+return theme
