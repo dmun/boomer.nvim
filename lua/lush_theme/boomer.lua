aa2c8c4727c7722239db1b2437b0bd1635c598a6
David van Munster <davidru01@live.nl>
2025-05-07 20:22:49 +0200

    fjeoiajfieopawf

--- lua/lush_theme/boomer.lua
+++ lua/lush_theme/boomer.lua
@@ -1,16 +1,17 @@
 local lush = require("lush")
 local hsluv = lush.hsluv
 
-local bg = hsluv(240, 30, 16)
-local fg = hsluv(220, 20, 99)
+local bg = hsluv(220, 80, 8)
+local fg = hsluv(220, 50, 100)
 local white = fg.li(80)
+local pink   = hsluv(340, 90, 70)
+local purple = hsluv(320, 50, 80)
+local blue   = hsluv(240, 90, 80)
+local cyan   = hsluv(200, 60, 75)
+local green  = hsluv(100, 80, 90)
 local yellow = hsluv(65, 90, 88)
-local orange = hsluv(40, 90, 75)
-local purple = hsluv(300, 50, 75)
-local cyan = hsluv(160, 70, 75)
-local green = hsluv(100, 80, 85)
-local blue = hsluv(240, 90, 75)
-local red = hsluv(10, 90, 60)
+local orange = hsluv(35, 90, 75)
+local red    = hsluv(10, 90, 65)
 
 ---@diagnostic disable: undefined-global
 local theme = lush(function(injected_functions)
@@ -28,22 +29,25 @@ local theme = lush(function(injected_functions)
 
     Cursor         { bg = white.da(20).de(20) },
     CursorLine     { bg = bg.li(5).de(10) },
-    LineNr         { fg = bg.li(15).de(20) },
+    LineNr         { fg = bg.li(40).de(40) },
     CursorLineNr   { fg = Cursor.bg },
     -- CursorLineSign { bg = bg.li(5).de(10) },
     Visual         { bg = bg.li(20).de(20) },
     Normal         { fg = fg, bg = bg },
     NormalFloat    { bg = bg.da(40).de(10) },
-    FloatBorder    { fg = fg.da(30).de(30), bg = NormalFloat.bg },
-    FloatTitle     { fg = FloatBorder.fg.li(30), bg = NormalFloat.bg },
-    StatusLine     { bg = bg.da(40).de(10), fg = fg },
-    StatusLineNC   { bg = bg.da(20), fg = fg.da(20).de(20) },
+    -- NormalFloat    {},
+    -- NormalAlt      { bg = bg.da(40).de(10) },
+    FloatBorder    { fg = fg.da(40).de(30), bg = NormalFloat.bg },
+    FloatTitle     { fg = FloatBorder.fg.li(80), bg = NormalFloat.bg, bold = true },
+    StatusLine     { bg = bg.da(40).de(20), fg = fg },
+    StatusLineNC   { StatusLine },
+    -- StatusLineNC   { bg = bg.da(20), fg = fg.da(20).de(20) },
     Tabline        { fg = bg, bg = StatusLineNC.bg },
     TablineSel     { bg = bg, fg = fg },
     WinSeparator   { FloatBorder },
-    Pmenu          { bg = bg.li(20).de(40) },
-    PmenuSel       { bg = blue.da(60).sa(60) },
-    PmenuSbar      { bg = Pmenu.bg.da(20).sa(20) },
+    Pmenu          { bg = bg.da(30) },
+    PmenuSel       { bg = Pmenu.bg.li(18).sa(20) },
+    PmenuSbar      { bg = Pmenu.bg.da(20) },
     PmenuThumb     { bg = Pmenu.bg.li(20).de(20) },
     QuickFixLine   { PmenuSel },
     DiagnosticInfo { fg = blue.li(20).de(20) },
@@ -63,37 +67,44 @@ local theme = lush(function(injected_functions)
     Removed        { fg = red },
 
     -- Syntax
-    Delimiter      { fg = fg.da(20).de(80) },
+    Delimiter      { fg = fg },
     Punctuation    { fg = fg },
-    Keyword        { fg = purple },
-    Operator       { Keyword },
+    Keyword        { fg = cyan },
+    Operator       { fg = fg },
     Special        { fg = white },
-    Constant       { fg = yellow },
+    Constant       { fg = orange },
     Number         { Constant },
     PreProc        { Constant },
     String         { fg = green },
-    Identifier     { fg = fg },
-    Function       { fg = blue },
+    Identifier     { fg = white },
+    Function       { fg = yellow },
     Statement      { Keyword },
-    Type           { fg = yellow },
+    Type           { fg = orange },
     Directory      { fg = blue },
-    Comment        { fg = cyan },
+    Comment        { fg = fg.da(40).de(40) },
+    Conditional    { fg = orange },
 
-    sym"@variable" { fg = fg },
+    sym"@variable"          { Identifier },
+    sym"@variable.member"   { Identifier },
     sym"@lsp.type.modifier" { fg = red },
     sym"@keyword.modifier"  { fg = red },
+    sym"@keyword.operator"  { Conditional },
+    sym"@keyword.conditional" { Conditional },
+    sym"@keyword.repeat"    { Conditional },
     sym"@type"              { Type },
     sym"@constructor"       { Type },
     sym"@constructor.lua"   { Delimiter },
     sym"@type.builtin"      { Keyword },
     sym"@function"          { Function },
-    sym"@function.builtin"  { Keyword },
+    sym"@function.builtin"  { Function },
     sym"@character"         { fg = red },
-    sym"@tag"               { Constant },
-    sym"@tag.builtin"       { Keyword },
+    sym"@string.tsx"        { fg = blue },
+    Tag                     { fg = green },
+    -- sym"@tag"               { Keyword },
+    sym"@tag.builtin"       { Tag },
+    sym"@tag.attribute"     { fg = Tag.fg.de(40).li(20) },
     sym"@tag.delimiter"     { Delimiter },
     sym"@markup.heading.markdown"    { fg = red },
-    -- sym"@function.builtin"  { fg = Keyword.fg.ro(20).de(10) },
 
     -- Plugins
     CopilotSuggestion { fg = bg.li(50).de(50) },
@@ -106,6 +117,9 @@ local theme = lush(function(injected_functions)
     IlluminatedWordRead  { bg = CursorLine.bg.li(5).de(5)},
     IlluminatedWordText  { bg = CursorLine.bg.li(5).de(5)},
     IlluminatedWordWrite { bg = CursorLine.bg.li(5).de(5)},
+
+    -- FzfLuaBorder            { StatusLine },
+    -- FzfLuaTitle             { fg = blue, bg = StatusLine.bg },
 
     -- stylua: ignore end
   }
