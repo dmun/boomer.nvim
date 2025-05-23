6e07d85d013037d2fb7da8c327eff36631544c7d
David van Munster <davidru01@live.nl>
2025-05-23 13:42:45 +0200

    f

--- lua/lush_theme/boomer.lua
+++ lua/lush_theme/boomer.lua
@@ -3,16 +3,16 @@ local light = false
 local lush = require("lush")
 local hsluv = lush.hsluv
 
-local bg = hsluv(240, 50, 4)
+local bg = hsluv(245, 70, 7)
 local fg = hsluv(220, 20, 99)
 local white = fg.li(80)
-local yellow = hsluv(65, 80, 85)
-local orange = hsluv(50, 80, 75)
-local purple = hsluv(270, 90, 75)
+local yellow = hsluv(65, 90, 86)
+local orange = hsluv(50, 80, 80)
+local purple = hsluv(275, 90, 75)
 local cyan = hsluv(140, 80, 90)
 local green = hsluv(110, 70, 90)
 local blue = hsluv(230, 90, 80)
-local red = hsluv(10, 90, 65)
+local red = hsluv(5, 90, 60)
 
 local bg_light = hsluv(240, 30, 100)
 local fg_light = hsluv(220, 20, 0)
@@ -61,6 +61,7 @@ local theme = lush(function(injected_functions)
     RainbowDelimiterRed          { fg = red.da(10).sa(60) },
 
     Cursor         { bg = white.da(10).de(10) },
+    MultiCursorCursor { bg = white.da(40).sa(40) },
     CursorLine     { bg = bg.li(5).de(10) },
     LineNr         { fg = bg.li(20).de(20) },
     CursorLineNr   { fg = Cursor.bg },
@@ -69,23 +70,26 @@ local theme = lush(function(injected_functions)
     Visual         { bg = bg.li(20).de(20) },
     Normal         { fg = fg, bg = bg },
     NormalFloat    { bg = bg.da(40).de(10) },
-    FloatBorder    { fg = fg.da(30).de(30), bg = NormalFloat.bg },
-    FloatTitle     { fg = FloatBorder.fg.li(30), bg = NormalFloat.bg },
+    FloatBorder    { fg = fg.da(40).sa(10), bg = NormalFloat.bg },
+    -- FloatBorder    { fg = Normal.bg, bg = NormalFloat.bg },
+    FloatTitle     { fg = FloatBorder.fg.li(50), bg = NormalFloat.bg },
     StatusLine     { bg = bg.da(40).de(10), fg = fg.da(20).de(20) },
     StatusLineNC   { StatusLine },
     Tabline        { fg = fg.da(30).de(30), bg = StatusLineNC.bg },
     TablineSel     { bg = bg, fg = fg },
-    WinSeparator   { fg = bg.da(40).de(40) },
+    WinSeparator   { fg = "NONE", bg = bg.da(40) },
     VertSplit      { WinSeparator },
     SignColumn     { bg = Normal.bg },
     EndOfBuffer    { fg = bg.li(10).de(10) },
-    Pmenu          { bg = bg.da(30).de(30) },
-    PmenuSel       { bg = bg.li(20).sa(20) },
+    Pmenu          { bg = bg.li(10) },
+    PmenuSel       { bg = bg.li(25).sa(20) },
     PmenuSbar      { bg = Pmenu.bg.da(20).sa(20) },
     PmenuThumb     { bg = Pmenu.bg.li(20).de(20) },
     QuickFixLine   { PmenuSel },
-    DiagnosticInfo { fg = blue.li(20).de(20) },
     Folded         { bg = bg.li(10).de(10) },
+
+    DiagnosticInfo { fg = blue.li(20).de(20) },
+    DiagnosticFloatingInfo { fg = blue.li(20).de(20), bg = NormalFloat.bg },
     DiagnosticUnnecessary { fg = fg.da(40).de(40) },
 
     -- Git
@@ -109,9 +113,9 @@ local theme = lush(function(injected_functions)
     Constant       { fg = yellow },
     Number         { Constant },
     PreProc        { Constant },
-    String         { fg = blue.li(30) },
+    String         { fg = green },
     Identifier     { fg = fg },
-    Function       { fg = blue },
+    Function       { fg = orange },
     Statement      { Keyword },
     Type           { fg = orange },
     Tag            { fg = cyan },
@@ -145,14 +149,18 @@ local theme = lush(function(injected_functions)
     -- Plugins
     CopilotSuggestion { fg = bg.li(50).de(50) },
 
-    BlinkCmpSignatureHelp       { Pmenu },
-    BlinkCmpSignatureHelpBorder { Pmenu },
+    BlinkCmpDocBorder           { FloatBorder },
+    BlinkCmpDocSeparatorBorder  { FloatBorder },
+    BlinkCmpSignatureHelp       { NormalFloat },
+    BlinkCmpSignatureHelpBorder { FloatBorder },
 
     TreesitterContext { CursorLine },
 
     IlluminatedWordRead  { bg = CursorLine.bg.li(5).de(5)},
     IlluminatedWordText  { bg = CursorLine.bg.li(5).de(5)},
     IlluminatedWordWrite { bg = CursorLine.bg.li(5).de(5)},
+
+    fugitiveHunk { fg = fg.da(30) },
 
     -- stylua: ignore end
   }
